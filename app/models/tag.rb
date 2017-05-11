5cc946969016872ef7d52d0c1ffb340eb699d48d
github username <github email address>
2017-05-11 13:27:46 -0400

    trending tags dont repeat and recent comments dont show if no recent comments

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -14,6 +14,7 @@ class Tag < ApplicationRecord
 
 
       def self.trending
+        binding.pry
           @pictures = Picture.all[-10..-1]
 
          @sorted =  @pictures.sort_by do |pic|
@@ -22,7 +23,7 @@ class Tag < ApplicationRecord
         @results = @sorted.reverse.map do |picture|
               picture.tags
           end
-          @results.flatten!
+          @results.flatten!.uniq
       end
 
 
