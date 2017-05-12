118a5e12149df37dd06987d49d29cfbbb12d0b01
github username <github email address>
2017-05-12 14:32:39 -0400

    added update prof pic to user show

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,17 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: true
+  # validates :has_user_pic?
   has_many :pictures
   has_many :comments
 
+
+
+    def has_user_pic?
+      binding.pry
+      # if @user(params[:user_pic]).empty?
+      # end
+    end
 
     def received_comments
         @pictures = self.pictures
