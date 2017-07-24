11854cb7dfca02bc9743fd580d2f512a69e62be2
github username <github email address>
2017-07-24 16:15:34 -0400

    user pic at user create and update prof pic work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ before_action :authorize_user, only: [:index, :show]
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation, :user_pic)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :image)
   end
 
 end
