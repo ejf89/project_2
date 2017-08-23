0ba3a1256ab0a63a6e254a36d4f30446ff724ce1
ejf89 <eric.j.farber@gmail.com>
2017-08-23 15:06:31 -0400

    getridofpclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ApplicationRecord
 
   has_many :pictures
   has_many :comments
-  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/gif", "image/png"]
 
 
 
